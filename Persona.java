170ab9c237e1a2653f00db5a4bef4f14b0b4942c
Uziel Alejandro <uzielalejandro777@gmail.com>
2024-07-16 22:02:50 -0600

    Agregando clases Persona por Uziel Alejandro

---
+++ Persona.java
@@ -0,0 +1,31 @@
+abstract class Persona {
+    // Atributos privados para encapsulamiento
+    private String nombre;
+    private int edad;
+
+    // Constructor
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Métodos getter y setter para encapsulamiento
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Método abstracto que será implementado por las subclases
+    public abstract void mostrarInformacion();
+}
